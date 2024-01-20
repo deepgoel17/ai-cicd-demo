648d564615cabc68b86a5e6964e02df82406b90b
deepgoel17 <deepgoel@google.com>
2024-01-20 17:29:11 +0530

    jq test

--- publishIntegration.bash
+++ publishIntegration.bash
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-echo "Hello world!"
+
+ echo '{"person":{"name":"John", "age":30, "city":"New York"}}' | jq '.person.name'
+# urlUsCentral1="https://stagingqualuscentral1-integrations-googleapis.sandbox.google.com/v1"
+# publishURI="projects/standalone-ip-prod-testing/locations/us-central1/integrations/deep-connector-demo-config/versions/$1"
+
+#  curl -v -X GET -H "Content-Type: application/json" "$urlUsCentral1/$publishURI:downloadJsonPackage?files=INTEGRATION" -H "Authorization: Bearer $(gcloud auth print-access-token)";
